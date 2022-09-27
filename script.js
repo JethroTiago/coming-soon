fe4a482685d76ae5cf0c30feb0f23feeec891fbc
Jethro Tiago <jethrotiago@hotmail.com>
2022-09-27 10:51:25 -0300

    Added new line codes and form tests

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log('Projeto iniciado com sucesso!');
-addEventListener
+const email = document.getElementById('email');
+const btn = document.querySelector(".botao");
+
+btn.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+function checkEmail() {
+    const emailValue = email.value.trim();
+
+    if(emailValue = 'true') {
+        console.log('Email enviado!');
+    }
+}
